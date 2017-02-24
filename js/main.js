2a373ea3a71af8830847e78633d0652e933c21c5
Michał Chudecki <majk.chudecki@gmail.com>
2017-02-24 21:18:43 +0100

    modified

--- js/main.js
+++ js/main.js
@@ -47,6 +47,19 @@ $(document).ready(function(){
 });
 */
 
+/*
+$("#btn-gallery").click(function(e) {
+  $(".gallery-hidden").slideToggle("slow");
+  e.preventDefault();
+});
+
+$("#btn-blog").click(function(e) {
+  $(".blog-hidden").slideToggle();
+  e.preventDefault();
+});
+*/
+
+
 $(document).ready(function(){
 
   $("#btn-gallery").click(function(e) {
@@ -70,19 +83,72 @@ $(document).ready(function(){
   });
 });
 
+$(document).ready(function(){
+
+  var home = $('#home').offset().top;
+  var aboutus = $('#about-us').offset().top;
+  var ourservices = $('#our-services').offset().top;
+  var gallery = $('#gallery').offset().top;
+  var subscribe = $('#subscribe').offset().top;
+  var blog = $('#blog').offset().top;
+  var contact = $('#contact').offset().top;
+
+  $(document).scroll(function() {
+    if ($(document).scrollTop() >= home && $(document).scrollTop() < aboutus) {
+      $('.navbar-default').css('background-color', 'transparent');
+    } else if ($(document).scrollTop() >= aboutus && $(document).scrollTop() < ourservices) {
+      $('.navbar-default').css('background-color', '#00c4c4');
+    } else if ($(document).scrollTop() >= ourservices && $(document).scrollTop() < gallery) {
+      $('.navbar-default').css('background-color', '#70bfff');
+    } else if ($(document).scrollTop() >= gallery && $(document).scrollTop() < subscribe) {
+      $('.navbar-default').css('background-color', '#ffe92f');
+    } else if ($(document).scrollTop() >= subscribe && $(document).scrollTop() < blog) {
+      $('.navbar-default').css('background-color', 'transparent');
+    } else if ($(document).scrollTop() >= blog && $(document).scrollTop() < contact) {
+      $('.navbar-default').css('background-color', '#b07cf7');
+    } else if ($(document).scrollTop() >= contact) {
+      $('.navbar-default').css('background-color', 'transparent');
+    }
+  });
+});
+
 
 
 /*
-$("#btn-gallery").click(function(e) {
-  $(".gallery-hidden").slideToggle("slow");
-  e.preventDefault();
+$(document).ready(function() {
+  var $window = $(window);
+  var $header = $('.navbar');
+  var sections = document.querySelectorAll('.header-color');
+
+  function findCurrentSection() {
+    var scrollTop = $window.scrollTop();
+    var height = $window.height();
+    
+    return Array.prototype
+      .find.call(sections, function(element) {
+        var offset = element.getBoundingClientRect();
+        var bottom = scrollTop + height;
+        return offset.top <= scrollTop && bottom <= offset.bottom;
+      });
+  }
+  
+  function onScroll() {
+    var section = findCurrentSection();
+    
+    if (section) {
+      var color = section.dataset.color;
+      
+      $header.css({ backgroundColor: color });
+    }
+  }
+
+  $window.scroll(onScroll);
 });
 
-$("#btn-blog").click(function(e) {
-  $(".blog-hidden").slideToggle();
-  e.preventDefault();
-});
 */
+
+
+/*
 
 $(document).ready(function(){
    var scroll_start = 0;
@@ -176,7 +242,7 @@ $(document).ready(function(){
   }
 });
 
-
+*/
 
 
 
